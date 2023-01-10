15722718bbbeb8742e3aa50edee17e6d03c29416
이름 <woong6041@naver.com>
2023-01-10 19:51:26 +0900

    시각화

--- streamlit.py
+++ streamlit.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly_express as px
+import plotly.express as px
 import plotly.graph_objects as go
 	
 st.title('Data visulization hosting')
